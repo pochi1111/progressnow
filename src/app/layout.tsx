300ab06459298df14b7a7b6a0d95f0e3572eeb6b
黒狗さん。 <kuroinusanshinji@gmail.com>
2025-04-09 20:20:07 +0900

    [add] add base header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Header } from "@/components/layouts/header";
 import {
   ColorSchemeScript,
   mantineHtmlProps,
@@ -8,8 +9,11 @@ import theme from "./theme";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Next App Mantine Tailwind Template",
-  description: "Next App Mantine Tailwind Template",
+  title: "Progress now",
+  description: "project progress management tool",
+  icons: {
+    icon: "/icon/icon.svg",
+  },
 };
 
 export default function RootLayout({
@@ -18,12 +22,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" {...mantineHtmlProps}>
+    <html lang="ja" {...mantineHtmlProps}>
       <head>
         <ColorSchemeScript />
       </head>
       <body className="antialiased">
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+        <MantineProvider theme={theme}>
+          <Header />
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
